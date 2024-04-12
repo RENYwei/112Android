e39b8ec0716048aec3252bf80fdecfe55ebec665
RENYwei <a109222018@mail.shu.edu.tw>
2024-04-12 14:59:06 +0800

    2024/04/12

---
+++ CH5_4/app/src/main/java/com/example/ch5_4/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.ch5_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.service.autofill.SavedDatasetsInfo;
+import android.view.KeyEvent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //public boolean onKeyDown(int keycode, KeyEvent event){
+    //    super.onCreate(S);
+    //}
+}
